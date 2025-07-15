3961dba2821449f7c79595bb26a339ec3556f248
Mr-Right-Dev <diego.sec.pay@gmail.com>
2025-07-15 20:30:11 -0300

    even fix

--- list-source/main.js
+++ list-source/main.js
@@ -31,6 +31,22 @@ function loadComplete() {
     loader.classList.add("hide");
 }
 
+function alternateItemColors() {
+    const items = Array.from(document.querySelectorAll('.item'));
+    let visibleIndex = 0;
+    
+    for (const item of items) {
+        item.classList.remove('even');
+        if (!item.classList.contains('hidden')) {
+            if (visibleIndex % 2 === 1) {
+                item.classList.add('even');
+            }
+            visibleIndex++;
+        }
+    }
+}
+
+
 function countItems(arr) {
     let count = 0;
     for (const obj of arr) {
@@ -72,6 +88,7 @@ let totalFiles = countItems(localSettings.files);
 
 function connectFolder(folderEl, childrenKey) {
     folderEl.addEventListener("click", () => {
+        
         const expanded = folderEl.getAttribute("expanded") === "true";
         folderEl.setAttribute("expanded", String(!expanded));
         folderEl.querySelector(".icon").src = `list-source/imgs/generic/${expanded ? "folder-closed" : "open-folder"}.png`;
@@ -79,6 +96,7 @@ function connectFolder(folderEl, childrenKey) {
         for (const child of htmlStock[childrenKey] || []) {
             child.classList.toggle("hidden");
         }
+        alternateItemColors();
     });
 }
 
@@ -151,6 +169,7 @@ function parse(arr, parentKey = "root", depth = 0) {
 function load() {
     load_ctx.innerHTML = `Injecting html (${totalLoaded}/${totalFiles})`;
     parse(localSettings.files);
+    alternateItemColors();
     load_ctx.innerHTML = "Injecting complete.";
     setTimeout(loadComplete, 100);
 }
