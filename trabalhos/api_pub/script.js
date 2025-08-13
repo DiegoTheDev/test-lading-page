536fde5d61ce725855f63dd7212c64c9cd09ecaa
Mr-Right-Dev <diego.sec.pay@gmail.com>
2025-08-13 10:47:03 -0300

    more

--- trabalhos/api_pub/script.js
+++ trabalhos/api_pub/script.js
@@ -1,7 +1,10 @@
 var parsedDom = [];
 
 function spawn(imgSrc) {
+    let breed = imgSrc.split("/")[4]
+    let name = breed.replaceAll("-", " ");
     let injectionHtml = ` <div class="card hidden">
+            <h3 class="dog-name">${name}</h3>
             <img src="${imgSrc}" class="loading">
             <div class="elmt">
                 <h3>Attaque: </h3>
@@ -53,9 +56,16 @@ async function inject() {
                  let temp = parsedDom[i];
                 setTimeout(() => {
                    temp.classList.remove("hidden");
-                    setTimeout(() => {
+                   let vals = [];
+                   setTimeout(() => {
                        temp.querySelectorAll(".elmt").forEach(element => {
-                            let val = Math.random()*100;
+                            let val = Math.random()*108;
+                            if (val >= 90) {
+                                if (Math.random() > 0.5) {
+                                    val -= Math.random()*30;
+                                }
+                            }
+                            vals.push(val);
                             let txt = element.querySelector(".placeholder h3");
                             txt.innerHTML = val.toFixed(2)
                             if (val > 80) {
@@ -66,7 +76,27 @@ async function inject() {
                                 txt.style.color = "red";
                             }
                             element.querySelector(".placeholder").classList.remove("active");
+
+                            
                         });
+                        let num = 0;
+                        for (let e = 0; e < vals.length; e++) {
+                            num+= vals[e];
+                        }
+
+                        num = num/vals.length;
+                        let txt = temp.querySelector(".dog-name");
+                        if (num > 100) {
+                            txt.innerHTML += "<br>(GOD)";
+                            txt.style.color = "#8A2BE2";
+                        } else if (num > 80) {
+                            txt.style.color = "#8A2BE2";
+                        } else if (num > 50) {
+                             txt.style.color = "green";
+                        } else {
+                            txt.style.color = "red";
+                        }
+                        txt.innerHTML += `<br>[${num.toFixed(2)}]`;
                     }, Math.random()*2000)
                 }, i*100)
             }
